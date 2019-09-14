1d63cebe05b8f81eb5fffac92749b427d92a40ee
Galih Mahendra Wicaksono 62 <mahendragalih26@gmail.com>
2019-09-14 12:58:15 +0700

    add more function

--- app.js
+++ app.js
@@ -3,25 +3,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
-
+const bodyParser = require("body-parser");
 const Route = require("./src/routes/root");
-// const Cloudinary = require('./src/config/cloudinaryConfig')
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on Port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}`);
 });
 
-app.use(express.static(path.resolve(__dirname, "src/public")));
-// app.use('*', Cloudinary.cloudinaryConfig)
-
-app.use(cors());
 app.use(logger("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", Route);
+
+module.exports = app;
